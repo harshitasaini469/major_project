9abb31e19469a754166f0c37f18798d396d5e728
Harshita <harshitasaini469@gmail.com>
2023-12-14 17:01:17 +0530

    made changes in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <nav className="bg-green-600 h-20 flex gap-3 items-center  pl-5 ">
-      <img src="./logo.png" alt="" className='w-14 h-14'/>
-        <p className="text-white font-semibold text-2xl  ">OptiMillet</p>
+      <img src="./logo.png" alt="" className='w-14 h-14 ml-16'/>
       </nav>
   )
 }
